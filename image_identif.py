90adc1ce7016d2838c586d3069ec46c7ae94fdc8
Timothy Wang <33815744+timothywang03@users.noreply.github.com>
2022-11-06 03:11:16 -0500

    classification finish

--- image_identif.py
+++ image_identif.py
@@ -8,13 +8,13 @@ from model import MyModel
 from PIL import Image
 
 waste_types = ["cardboard", "glass", "metal", "paper", "plastic", "trash"]
-
+categorization = {'cardboard': 2, 'glass': 0, 'metal': 1, 'paper': 2, 'plastic': 0, 'trash': 1}
 
 def identify(image, model):
     # cnn2.pth model  was already pretrained before importing
     # dataset pulled from Stanford's dataset: https://github.com/garythung/trashnet
+    """Classifies images into one of the six waste types, then translates it
+        into recycling (0), landfill (1), or compost (2)
+        Also returns the certainty of the object"""
     inference, confidence = model.infer(image)
-    return waste_types[inference], confidence * 1000
-
-
-identify(image, model)
+    return categorization[waste_types[inference]], confidence * 100
