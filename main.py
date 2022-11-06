5ef3e8e43d935fd65e8b254e7adee0cfe648efdd
Timothy Wang <33815744+timothywang03@users.noreply.github.com>
2022-11-05 22:17:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+# Training and pretrained model taken from:
+# U4 Project - Deep Learning for Business - AI&BA - TBS Antoine Settelen,
+# Edgar Jullien, Simon Weiss
+# Cited Project Name: AI Waste Sorting using transfer learning with Pytorch
+# Pulled from: https://github.com/13w13/AI-Waste-Sorting-Web-App-Pytorch
+
 from model import MyModel
+import ui
